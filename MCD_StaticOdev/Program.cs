1c85b66d96d48c1b51de6b4e8983b88c370630cc
Faik Turan <turanfaik@hotmail.com>
2021-12-01 17:28:17 +0300

    Add project files.

---
+++ MCD_StaticOdev/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_StaticOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Müşteri adında bir nesne oluşturalım ve oluşturmuş olduğumuz nesnemizin içerisinde static olarak bir ArrayList oluşturalım.
+             * 
+             * Oluşturmuş olduğumuz ArrayList koleksiyonumuzu sanal bir database mantığında kullanacağız.
+             * Müşteri sınıfımızın içerisine
+             * ID, Isim, Soyisim, EmailAdres, KullaniciAdi, Sifre isimleri ile fieldkar oluşturalım.
+             * 
+             * Oluşturmuş olduğumuz bu field listesinden bazılarının kapsülleme filtreleri aşağıdaki gibi olmalodor.
+             * 
+             * Kullanici adı değeri sanal olarak oluşturmuş olduğumuz database imiz içerisinde aranan eğer kullanıcı adı sanal database içerisinde bulunursa field i.erisine eklemöek istenen değer eklenemesin(Kapsülleme)
+             * 
+             * Müşteri ekle adında static metot oluşturalım parametre olarak  müşteri tipi alsın içerisinde
+             * Müşteri nesnesinin null okup olmadığını ve müşteri nesnesi içerisindeki kullanıcı adının dolu olması şartına baksın.
+             * Yukarıdaki kriterlerden geçerse müşterimizi sanal olarak oluşturduğumuz database imize eklemeden önce email adresinden bir kontrol sağlasın eğer sistemde aynı email adresi ile müşteri değeri var ise eklemesin yok ise sanal database içerisine ilgili nesneyi eklesin.
+             * 
+             * 
+             * 
+             * 
+             */ 
+        }
+    }
+}
